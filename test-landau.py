4ea5be2604cfde036f248e51ac28033fcc062f87
Noel Dawe <noel.dawe@cern.ch>
2012-03-07 01:08:19 -0800

    supply params to test script

--- test-landau.py
+++ test-landau.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('-mu', type=float, default=.5)
+parser.add_argument('-c', type=float, default=2)
+args = parser.parse_args()
+
 from matplotlib import pyplot as plt
 import numpy as np
 from landau import denlan, diflan
@@ -7,7 +14,7 @@ from landau import denlan, diflan
 X = np.arange(0,10,.001)
 
 plt.figure()
-plt.plot(X, [diflan(x,.5,2) for x in X], label="diflan")
-plt.plot(X, [denlan(x,.5,2) for x in X], label="denlan")
+plt.plot(X, [diflan(x, args.mu, args.c) for x in X], label="diflan")
+plt.plot(X, [denlan(x, args.mu, args.c) for x in X], label="denlan")
 plt.legend()
 plt.show()
